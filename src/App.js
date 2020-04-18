a17b049f7c0b6b02ec0cb1445ea9e1c4e57748df
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-04-18 12:00:00 +0900

    fix: BookContextProvider component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.css";
+import BookContextProivider from "./contexts/BookContext";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BookContextProivider></BookContextProivider>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/contexts/BookContext.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+
+export const BookContext = createContext();
+
+const BookContextProivider = (props) => {
+  console.log(props);
+  const [books, setBooks] = useState([
+    { title: "name of the wind", author: "patrick rothfuss", id: 1 },
+    { title: "the final empire", author: "brandon sanderson", id: 2 },
+  ]);
+
+  const addBook = (title, author) => {
+    setBooks([...books, { title, author, id: uuidv4() }]);
+  };
+
+  const removeBook = (id) => {
+    setBooks(books.filter((book) => book.id !== id));
+  };
+
+  return (
+    <BookContext.Provider value={{ books, addBook, removeBook }}>
+      {props.chidlren}
+    </BookContext.Provider>
+  );
+};
+
+export default BookContextProivider;
