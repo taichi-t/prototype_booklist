2a11dbe9a2fe162235902ff9269d8280d099eeb1
Tsujioka Taichi <t.tujioka.0925@gmail.com>
2020-04-21 12:00:00 +0900

    create: BookDetails and BookList components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import BookContextProivider from "./contexts/BookContext";
 import Navbar from "./components/Navbar";
+import { BookList } from "./components/BookList";
 
 function App() {
   return (
     <div className="App">
       <BookContextProivider>
         <Navbar />
+        <BookList />
       </BookContextProivider>
     </div>
   );
